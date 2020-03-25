4868cca1ffc2fab524ed4953fa064b1a79ff8fe5
Gerald Versluis <gerald@verslu.is>
2020-03-25 14:02:14 +0100

    Initial commit

---
+++ EmbeddedFontsSample/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("Samantha.ttf")]
+[assembly: ExportFont("FA5Regular.otf", Alias = "FontAwesome")]
+
+namespace EmbeddedFontsSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
